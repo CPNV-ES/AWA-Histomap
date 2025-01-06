ec86debd5898d8c28c3e0b52bcd667aad7fca235
Benoit <47849605+BPTdev@users.noreply.github.com>
2025-01-06 14:40:54 +0100

    feat: add cards up and down

---
+++ src/components/cards/Card.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="h-[270px] w-[400px] mx-36">
+      <!-- Conteneur principal relatif pour positionner les enfants -->
+      <div class="h-full w-full relative rounded-lg overflow-hidden hover:scale-105 transition-all duration-500">
+        <!-- Image -->
+        <img :src="img_src" alt="cat" class="rounded-lg blur-lg">
+        <!-- Contenu superposé -->
+        <div class="absolute inset-0 flex flex-col justify-center items-center bg-black/30 p-5 text-white">
+          <div class="w-full h-1/5">
+            <h1>{{ title }}</h1>
+          </div>
+          <hr class="w-full bg-white/30 my-1 mb-2">
+          <div class="w-full h-4/5">
+            <p>{{ content }}</p>
+          </div>
+        </div>
+      </div>
+      <!-- Section décorative -->
+      <Line />
+    </div>
+  </template>
+
+  <script>
+  import Line from "@/components/cards/Line.vue";
+  export default {
+    props: {
+      img_src: {
+        type: String,
+        required: true,
+      },
+      title: {
+        type: String,
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      height: {
+        type: String,
+        default: "400px",
+      },
+      width: {
+        type: String,
+        default: "400px",
+      },
+    },
+  };
+  </script>
+
+  <style>
+  /* Ajout optionnel de styles globaux */
+  </style>

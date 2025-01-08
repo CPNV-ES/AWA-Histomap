69919c02f83f9cd1e553ec76b553e30ffc6cf2a3
vicmtn <victorien.montavon@gmail.com>
2025-01-08 09:53:39 +0100

    feat: Add refactored modal info App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script>
 //import CardShort from './components/CardShort.vue';
 import Card from '@/components/card.vue'
+import AddModal from '@/components/AddModal.vue';
 
 export default {
   name: 'App',
@@ -8,11 +9,26 @@ export default {
     Card,
   },
 };
+
+const modalOpen = ref(false);
+
+const handleSave = (data) => {
+  console.log('Saved data:', data);
+  // TODO: create saving method
+
+};
+
+onMounted(fetchContent);
 </script>
 
 <template>
   <div id="app" class="md:container">
     <Card />
+      <button @click="modalOpen = true" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded">
+        Add New Item
+      </button>
+      <AddModal :isOpen="modalOpen" @close="modalOpen = false" @save="handleSave" />
+    </div>
   </div>
 </template>
 

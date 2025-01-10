8db6c7fee2f23486b5a11ca7a866c220e1c89e91
vicmtn <victorien.montavon@gmail.com>
2025-01-10 20:03:16 +0100

    refactor: Refactor App.vue to use optionAPI isntead of composed api

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,58 @@
-<script setup>
-import { ref, onMounted } from 'vue';
-import AddModal from '@/components/AddModal.vue';
-import Card from '@/components/Card.vue';
+<script>
+import AddModal from '@/components/AddModal.vue'
+import Card from '@/components/card.vue'
 
-const stories = ref([]); // Liste des histoires
+//const stories = []; // Liste des histoires
+
+export default {
+  name: 'App',
+  components: {
+    AddModal,
+    Card
+  },
+  methods: {
+    CreateStory(newStory) {
+      console.log('New story:', newStory)
+    },
+    async fetchContent() {
+      try {
+        const response = await fetch('http://localhost:3000/stories')
+        this.stories = await response.json()
+        console.log('Contenus récupérés :', this.stories)
+      } catch (error) {
+        console.error('Erreur lors de la récupération des contenus :', error)
+      }
+    },
+    handleCloseModal() {
+      this.modalOpen = false
+      this.fetchContent()
+    },
+  },
+  data() {
+    return {
+      modalOpen: false,
+      stories: [],
+    }
+  },
 
 
-
-const fetchContent = async () => {
-  try {
-    const response = await fetch('http://localhost:3000/content');
-    stories.value = await response.json();
-  } catch (error) {
-    console.error('Erreur lors de la récupération des contenus :', error);
-  }
-};
-
-const modalOpen = ref(false);
-
-const handleSave = (data) => {
-  console.log('Saved data:', data);
-  // TODO: create saving method
-
-};
-
-onMounted(fetchContent);
+  mounted() {
+    this.fetchContent()
+  },
+}
 </script>
 
 <template>
   <div>
     <h1 class="text-center text-2xl font-bold mb-4">Timeline Historique</h1>
     <div class="p-4">
-      <button @click="modalOpen = true" class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded">
+      <button
+        @click="modalOpen = true"
+        class="bg-indigo-500 hover:bg-indigo-600 text-white font-bold py-2 px-4 rounded"
+      >
         Add New Item
       </button>
-      <AddModal :isOpen="modalOpen" @close="modalOpen = false" @save="handleSave" />
+      <AddModal :isOpen="modalOpen" @close="handleCloseModal" @createStory="CreateStory" />
     </div>
 
     <!-- Liste des histoires affichées avec le composant Card -->
@@ -50,6 +68,4 @@ onMounted(fetchContent);
   </div>
 </template>
 
-<style>
-
-</style>
+<style></style>

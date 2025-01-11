471e3344df55e0277edae5fcce63df34656e417e
vicmtn <victorien.montavon@gmail.com>
2025-01-11 18:46:44 +0100

    refactor: Remove unused block of code

--- src/components/addModal.vue
+++ src/components/addModal.vue
@@ -69,18 +69,6 @@ export default {
     closeModal() {
       this.$emit('close')
     },
-    /*
-        formatDate() {
-          const today = new Date();
-          const yyyy = today.getFullYear();
-          let mm = today.getMonth() + 1; // Months start at 0!
-          let dd = today.getDate();
-
-          if (dd < 10) dd = '0' + dd;
-          if (mm < 10) mm = '0' + mm;
-
-          const formattedToday = dd + '/' + mm + '/' + yyyy;
-        }*/
 
     validateForm() {
       if (this.formData.title.length < 5) {
